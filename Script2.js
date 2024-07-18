38a40f421ac19a86e340b0dfe46bc05616259e5e
Ivan_RegRock <47649807+Ivanfeliper@users.noreply.github.com>
2024-07-18 00:21:27 -0600

    AWS Game for IT 355 to Script2.js

---
+++ Script2.js
@@ -0,0 +1,69 @@
+// script.js
+function checkAnswer(selectedAnswer) {
+    const correctAnswer = "True"; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+function checkAnswer2(selectedAnswer) {
+    const correctAnswer = "Managing updates, patches, and logical firewall."; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+function checkAnswer3(selectedAnswer) {
+    const correctAnswer = "Pass the risk to a third-party company."; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+function checkAnswer4(selectedAnswer) {
+    const correctAnswer = "Default account created when first creating an amazon account."; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+function checkAnswer5(selectedAnswer) {
+    const correctAnswer = "Virtual Private Cloud is used to create networks within a cloud."; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+function checkAnswer6(selectedAnswer) {
+    const correctAnswer = "Amazon Machine Images. Amazon templates machine image that contains the configuration for applications."; // Set the correct answer here
+    const userAnswer = selectedAnswer.textContent;
+
+    if (userAnswer === correctAnswer) {
+        selectedAnswer.style.backgroundColor = "#c1f7d9"; // Light green for correct answer
+    } else {
+        // Handle incorrect answer (you can add feedback or other actions here)
+    }
+}
+
+
+
